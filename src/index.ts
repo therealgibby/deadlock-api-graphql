9096554f3a2635461721f9d6c3286f143450a1ef
therealgibby <raulgarcia987@gmail.com>
2025-08-02 01:35:38 -0500

    fix const PORT usage

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ const PORT = process.env.PORT || 4000;
 const schema = createSchema({ typeDefs: typeDefs, resolvers: resolversBuilder() });
 const yoga = createYoga({ schema });
 const server = createServer(yoga);
-server.listen(4000, () => {
+server.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}/graphql`);
 });
